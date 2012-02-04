db75d0e85575aa33764ade5ebd8bfad2922248ab
slouis <louis.sebastien@gmail.com>
2012-02-04 16:04:09 +0100

    api

--- app/controllers/ControllerParent.java
+++ app/controllers/ControllerParent.java
@@ -4,14 +4,20 @@ import java.util.ArrayList;
 import java.util.Properties;
 
 import models.User;
-import controllers.exploit.*;
 import play.Play;
 import play.data.validation.Required;
-import play.mvc.Before;
 import play.mvc.Controller;
 import play.mvc.Http;
 import play.mvc.Http.Request;
-
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import play.mvc.ActionInvoker;
+import play.utils.Java;
+import java.util.List;
+import play.mvc.After;
+import play.mvc.Before;
+import play.mvc.Finally;
+import play.mvc.With;
 
 public class ControllerParent extends SecurityPlugin {
 	
@@ -27,5 +33,62 @@ public class ControllerParent extends SecurityPlugin {
 			hd.values.add("*"); 
 			Http.Response.current().headers.put("Access-Control-Allow-Origin",hd);		
 		//}
-	}	
+	}
+	
+	private static class ClassObject {
+		String name;
+		List<MethodObject> methods = new ArrayList<MethodObject>();
+	}
+	
+	private static class MethodObject {
+		String name;
+		String returnType;
+		List<ArgObject> args = new ArrayList<ArgObject>();		
+	}
+	
+	private static class ArgObject {
+		String name;
+		String type;
+	}
+	
+	public static void api(){
+		
+		ClassObject classObject = new ClassObject();
+		Class currentController = getControllerClass();
+		classObject.name = currentController.getName();
+		List<Method> methods = findActionMethods(currentController);
+	
+		for (Method method : methods) {
+		  	MethodObject methodObject = new MethodObject();
+			methodObject.name = method.getName();
+			methodObject.returnType = Java.rawJavaType(method.getReturnType());
+			//Get args :
+			for (Class clazz : method.getParameterTypes()) {
+		        ArgObject args = new ArgObject();
+				//args.name = variable.getName();
+				args.type = Java.rawJavaType(clazz);
+				methodObject.args.add(args);
+		    }
+			
+			classObject.methods.add(methodObject);
+		}
+		renderJSON(classObject);
+	}
+	
+	private static List<Method> findActionMethods(Class clazz) {
+		List<Method> methods = new ArrayList<Method>();
+        while (!clazz.getName().equals("java.lang.Object")) {
+            for (Method m : clazz.getDeclaredMethods()) {
+                if (Modifier.isPublic(m.getModifiers())) {
+                    // Check that it is not an intercepter
+                    if (!m.isAnnotationPresent(Before.class) && !m.isAnnotationPresent(After.class) && !m.isAnnotationPresent(Finally.class)) {
+                        methods.add(m);
+                    }
+                }
+            }
+            clazz = clazz.getSuperclass();
+        }
+        return methods;
+    }
+		
 }
